63d54960dc1389e22b171c1da83cc3fb2a744a0c
grm <golosov_SA@mail.ru>
2022-09-27 16:10:34 +0300

    fix some typings

--- helpers/query_commands.py
+++ helpers/query_commands.py
@@ -6,28 +6,28 @@ from operator import itemgetter
 from typing import Iterator, Any, List, Union, Iterable
 
 
-def query_filter(data: Union[Iterator[str], Iterable[str]], value: str) -> Union[Iterator[str], Iterable[str]]:
+def query_filter(data: Union[Iterator[str], Iterable[str]], value: str) -> Iterator[str]:
     return filter(lambda s: value in s, data)
 
 
-def query_map(data: Union[Iterator[str], Iterable[str]], value: str) -> Union[Iterator[str], Iterable[str]]:
+def query_map(data: Union[Iterator[str], Iterable[str]], value: str) -> Iterator[str]:
     return map(lambda s: s.split()[int(value)], data)
 
 
-def query_unique(data: Union[Iterator[str], Iterable[str]], value: Any) -> Union[Iterator[str], Iterable[str]]:
+def query_unique(data: Union[Iterator[str], Iterable[str]], value: Any) -> Iterable[str]:
     return set(data)
 
 
-def query_sort(data: Union[Iterator[str], Iterable[str]], value: str) -> Union[Iterator[str], Iterable[str]]:
+def query_sort(data: Union[Iterator[str], Iterable[str]], value: str) -> Iterable[str]:
     if value not in ["asc", "desc"]:
         raise ValueError("Wrong sort value")
     return sorted(data, reverse=value == "desc")
 
 
-def query_limit(data: Union[Iterator[str], Iterable[str]], value: str) -> Union[Iterator[str], Iterable[str]]:
+def query_limit(data: Union[Iterator[str], Iterable[str]], value: str) -> Iterator[str]:
     return map(itemgetter(0), zip(data, range(int(value))))
 
 
-def query_regex(data: Union[Iterator[str], Iterable[str]], value: str):
+def query_regex(data: Union[Iterator[str], Iterable[str]], value: str) -> Iterator[str]:
     regex = re.compile(value)
-    return filter(lambda s: regex.search(s) is not None, data)
+    return filter(lambda s: regex.search(s), data)
